7a1485e7d36b10f610f4fc37445d16e7a16c5632
ryuusama <p.k17173@163.com>
2024-01-29 20:36:49 +0800

    lab9 finished

--- lab9/lab9/MyHashMap.java
+++ lab9/lab9/MyHashMap.java
@@ -98,9 +98,8 @@ public class MyHashMap<K, V> implements Map61B<K, V> {
     @Override
     public Set<K> keySet() {
         Set<K> set = new HashSet<>();
-        for (int i = 0; i < buckets.length; i += 1) {
-            Set<K> temp = buckets[i].keySet();
-            set.addAll(temp);
+        for (int i = 0; i < size; i++) {
+            set.addAll(buckets[i].keySet());
         }
         return set;
     }
