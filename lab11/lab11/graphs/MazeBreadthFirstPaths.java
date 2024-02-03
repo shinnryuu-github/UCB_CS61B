1133a903483e9905740043f88096c26562035a2c
ryuusama <p.k17173@163.com>
2024-02-03 16:41:10 +0800

    lab11 basically finished

--- lab11/lab11/graphs/MazeBreadthFirstPaths.java
+++ lab11/lab11/graphs/MazeBreadthFirstPaths.java
@@ -1,5 +1,9 @@
 package lab11.graphs;
 
+import edu.princeton.cs.algs4.MinPQ;
+
+import java.util.Comparator;
+
 /**
  *  @author Josh Hug
  */
@@ -9,21 +13,76 @@ public class MazeBreadthFirstPaths extends MazeExplorer {
     public int[] edgeTo;
     public boolean[] marked;
     */
+    private Vertex s;
+    private Vertex t;
+    private boolean targetFound = false;
+    private Maze maze;
 
+    private class Vertex implements Comparable<Vertex>{
+        int No, dist;
+        boolean marked;
+        Maze maze;
+        public Vertex(int x, int y, int d, boolean m, Maze M){
+            maze = M;
+            marked = m;
+            dist = d;
+            No = M.xyTo1D(x,y);
+        }
+        public Vertex(int num, int d, boolean m, Maze M){
+            maze = M;
+            marked = m;
+            dist = d;
+            No = num;
+        }
+        @Override
+        public int compareTo(Vertex o){
+            return dist - o.dist;
+        }
+    }
     public MazeBreadthFirstPaths(Maze m, int sourceX, int sourceY, int targetX, int targetY) {
         super(m);
         // Add more variables here!
+        maze = m;
+        s = new Vertex(sourceX, sourceY, 0, true, m);
+        t = new Vertex(targetX, targetY, m.N() * m.N(), true, m);
+        distTo[s.No] = s.dist;
+        edgeTo[s.No] = s.No;
     }
 
     /** Conducts a breadth first search of the maze starting at the source. */
     private void bfs() {
         // TODO: Your code here. Don't forget to update distTo, edgeTo, and marked, as well as call announce()
+        marked[s.No] = s.marked;
+        announce();
+        if (s.No == t.No) {
+            targetFound = true;
+        }
+        if (targetFound) {
+            return;
+        }
+        MinPQ<Vertex> pq = new MinPQ<>();
+        pq.insert(s);
+        while (!pq.isEmpty()){
+            Vertex newvertex = pq.delMin();
+            for (int vertex_num : maze.adj(newvertex.No)){
+                if (!marked[vertex_num]) {
+                    marked[vertex_num] = true;
+                    edgeTo[vertex_num] = newvertex.No;
+                    distTo[vertex_num] = newvertex.dist + 1;
+                    announce();
+                    if (vertex_num == t.No)
+                        return;
+                    Vertex adj = new Vertex(vertex_num, newvertex.dist + 1, true, maze);
+                    pq.insert(adj);
+                }
+            }
+        }
     }
 
 
     @Override
     public void solve() {
-        // bfs();
+        bfs();
     }
 }
 
--- lab11/lab11/graphs/MazeCycles.java
+++ lab11/lab11/graphs/MazeCycles.java
@@ -9,16 +9,43 @@ public class MazeCycles extends MazeExplorer {
     public int[] edgeTo;
     public boolean[] marked;
     */
+    private boolean find;
+    private int[] nodesTo;
 
     public MazeCycles(Maze m) {
         super(m);
+        find = false;
+        nodesTo = new int[m.N() * m.N()];
     }
-
     @Override
     public void solve() {
         // TODO: Your code here!
+        dfs(0, -1);
     }
 
     // Helper methods go here
+    private void dfs(int v, int pre) {
+        if (find)
+            return;
+        marked[v] = true;
+        nodesTo[v] = pre;
+        announce();
+        for (int w : maze.adj(v)) {
+            if (marked[w] && w != pre){
+                find = true;
+                edgeTo[w] = v;
+                announce();
+                for (int x = v; x != w; x = nodesTo[x]){
+                    edgeTo[x] = nodesTo[x];
+                    announce();
+                }
+                return;
+            }
+        }
+        for (int w : maze.adj(v)) {
+            if (!marked[w])
+                dfs(w, v);
+        }
+    }
 }
 
